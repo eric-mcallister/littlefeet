eb28b4ff42e59fc3450d0b9d19130deccb6ba9a0
Lamia <emcallister98@gmail.com>
2026-03-21 20:21:46 -0500

    Added a go back to top functionality

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,15 @@ import MonthlyMenuView from '@/views/MonthlyMenuView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    }
+    return { top: 0, left: 0 }
+  },
   routes: [
     {
       path: '/',
